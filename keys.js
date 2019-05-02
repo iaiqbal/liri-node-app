d4f5bee464e705a0d0f872c30cd04586a016384a
Ibrahim Iqbal <iaiqbal@uh.edu>
2019-05-02 16:40:54 -0500

    variables created, about to use inquirer to create logic

--- keys.js
+++ keys.js
@@ -4,3 +4,26 @@ exports.spotify = {
     id: process.env.SPOTIFY_ID,
     secret: process.env.SPOTIFY_SECRET
 };
+
+
+
+
+
+
+exports.omdb = {
+    id: process.env.OMDB_ID
+};
+
+console.log(process.env.OMDB_ID);
+
+exports.ticketmaster = {
+    id: process.env.TICKETMASTER_ID,
+    secret: process.env.TICKETMASTER_SECRET
+};
+
+
+
+
+
+
+// const tmKey = process.env.TICKETMASTER_ID;
--- liri.js
+++ liri.js
@@ -2,4 +2,30 @@ require("dotenv").config();
 
 const keys = require("./keys.js");
 
-const spotify = new Spotify(keys.spotify);
+const fs = require("fs");
+
+// const spotifyKeys = new Spotify(keys.spotify);
+
+// const spotify = require("node-spotify-api");
+
+const request = require("request");
+
+// const omdb = require("omdb");
+const axios = require("axios");
+
+// const ticketmaster = require("ticketmaster");
+
+
+const movie = process.argv[2];
+const omdbQuery = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+// const omdbQuery = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=" + omdb;
+console.log(omdbQuery);
+
+
+axios.get(omdbQuery)
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
